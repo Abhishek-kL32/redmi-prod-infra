36d4b54f0aa2ec740d705f15874dd1d2d26a1f4c
root <root@ip-172-31-11-141.ap-south-1.compute.internal>
2023-12-07 08:03:48 +0000

    redmi initial commit

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "ami-id" {
+
+  type        = string
+  description = "default ami will be Amazon Linux"
+}
+
+variable "instance_type" {
+
+  type        = string
+  description = "Free tyre instance type"
+}
+
+variable "project_name" {
+
+  type        = string
+  description = "Name of Project"
+}
+
+variable "project_env" {
+
+  type        = string
+  description = "Project Environment"
+}
+
+variable "owner" {
+
+  type        = string
+  description = "Project Environment"
+}
