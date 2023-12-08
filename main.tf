6ce4b3d74c54c800370dd419724e2f5f930f0cf8
root <root@ip-172-31-11-141.ap-south-1.compute.internal>
2023-12-08 10:33:33 +0000

    elastic_ip for frontend

--- main.tf
+++ main.tf
@@ -109,6 +109,18 @@ resource "aws_instance" "frontend" {
 }
 
 ############################################
+# create and attach elastic-ip             #
+############################################
+
+resource "aws_eip" "frontend" {
+  instance = aws_instance.frontend.id
+  domain   = "vpc"
+  tags = {
+    "Name" = "${var.project_name}-${var.project_env}-frontend"
+  }
+}
+
+############################################
 # create record for webserver              #
 ############################################
 
@@ -118,6 +130,6 @@ resource "aws_route53_record" "webserver" {
   name    = "${var.hostname}.${var.hosted_zone_name}"
   type    = "A"
   ttl     = 300
-  records = [aws_instance.frontend.public_ip]
+  records = [aws_eip.frontend.public_ip]
 }
 
